00e05cd3f1f0a54b65c58ddb4c607fd256aa96f8
nniddnoy <73216249+nniddnoy@users.noreply.github.com>
2021-08-05 17:53:34 +0700

    Update v_register.php
    ทำพวก input ค่า

--- html/DQS/application/views/v_register.php
+++ html/DQS/application/views/v_register.php
@@ -26,15 +26,58 @@
             </div>
             <div class="col">
                 <div class="p-3 ">หน่วยงาน</div>
-                <select class="form-select" aria-label="Default select example">
-                    <option value="volvo">-- เลือกจังหวัด --</option>
-                    <option value="volvo">Volvo</option>
-                    <option value="saab">Saab</option>
-                    <option value="fiat">Fiat</option>
-                    <option value="audi">Audi</option>
-                </select>
+                    <select class="form-select" aria-label="Default select example">
+                        <option value="volvo">-- เลือกจังหวัด --</option>
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="fiat">Fiat</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                </div>
             </div>
+
+            <div class="row align-items-center">
+                <div class="col">
+                    <div class="p-3 ">รหัสพนักงาน</div>
+                    
+                        <input type="text" id="mem_emp_id" name="mem_emp_id"><br><br>
+                </div>
+
+                <div class="col">
+                    <div class="p-3 ">อีเมล</div>
+                        <input type="email" id="mem_email" name="mem_email"><br><br>
+                    </div>
+                </div>
             
+
+            
+            <div class="row align-items-center">
+                <div class="col">
+                    <div class="p-3 ">ชื่อ</div>
+                    
+                        <input type="text" id="mem_firstname" name="mem_firstname"><br><br>
+                </div>
+                <div class="col">
+                    <div class="p-3 ">นามสกุล</div>
+                        <input type="text" id="mem_lastname" name="mem_lastname"><br><br>
+                </div>
+                
+            </div>
+
+            
+            <div class="row align-items-center">
+                <div class="col">
+                    <div class="p-3 ">รหัสผ่าน</div>
+                    
+                        <input type="text" id="mem_password" name="mem_password"><br><br>
+                </div>
+                <div class="col">
+                    <div class="p-3 ">ยืนยันรหัสผ่าน</div>
+                        <input type="text" id="mem_password" name="mem_password"><br><br>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </form>
 </div>
