461f09b3a31b2824c946a7ca1d78c9ca99549f43
Domeasw <62160115@go.buu.com>
2021-08-07 14:57:58 +0700

    Update Member_login.php

--- html/DQS/application/controllers/Member/Member_login.php
+++ html/DQS/application/controllers/Member/Member_login.php
@@ -16,6 +16,21 @@ class Member_login extends DQS_controller {
 		$this->output_naevar("Member/v_member_home");
 	}
 
+	public function login()
+	{
+		$mem_username = $this->input->POST('mem_username');
+		$mem_password = $this->input->POST('mem_password');
 
+		$this->load->model('M_dqs_login','log');
+
+		$userlogin = $this->log->check_login($mem_username,$mem_password)->result();
+		if(count($userlogin)==1){
+			$_SESSION['mem_username'] = $this->input->post('mem_password');
+			echo true;
+		}else{
+			echo false;
+		}
+	}
+	
 }
 ?>
