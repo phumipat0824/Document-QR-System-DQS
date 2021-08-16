a7ed9e5bc5ff7018b9336c0d10d665b90f14918a
Domeasw <62160115@go.buu.com>
2021-08-16 14:32:47 +0700

    Update Member_login.php

--- html/DQS/application/controllers/Member/Member_login.php
+++ html/DQS/application/controllers/Member/Member_login.php
@@ -47,42 +47,6 @@ class Member_login extends DQS_controller
         $this->mem->mem_username =  $mem_username;
         $data['arr_member'] = $this->mem->get_all()->result();
 
-        // if ($obj_user == NULL) {
-        //     // log in failed
-        //     $data = [];
-        //     $data['error'] = "Username หรือ Password ไม่ถูกต้อง";
-        //     $data['mem_username'] = $mem_username;
-        //     $data['mem_password'] = $mem_password;
-        //     $this->load->view('Login/v_login', $data);
-        // } else {
-        //     // log in complete
-
-        //     // set id and name for user
-        //     $_SESSION['id'] = $obj_user->usr_id;
-        //     $_SESSION['name'] = $obj_user->usr_name;
-        //     $_SESSION['username'] = $obj_user->usr_username;
-        //     $_SESSION['year_month'] = date('Y-m');
-        //     $_SESSION['delete_error'] = '';
-        //     $_SESSION['edit_error'] = '';
-        //     $_SESSION['denied_error'] = '';
-        //     $_SESSION['approved_error'] = '';
-        //     $_SESSION['clear_state'] = 'true';
-
-        // if for 4 actors
-        // if ($obj_user->usr_role == 1) {
-        //     $_SESSION['role'] = 'requester';
-        //     redirect('Requester/Requisition_list/show_requisition_list');
-        // } else if ($obj_user->usr_role == 2) {
-        //     $_SESSION['role'] = 'approver';
-        //     redirect('Approver/Requisition_list_approver/show_requisition_list_approver');
-        // } else if ($obj_user->usr_role == 3) {
-        //     $_SESSION['role'] = 'accountant';
-        //     redirect('Accountant/Requisition_list_accountant/show_requisition_list_accountant');
-        // } else {
-        //     $_SESSION['role'] = 'admin';
-        //     redirect('Admin/Project_list/show_project_list');
-        // }
-        //}
         foreach ($data['arr_member'] as $key => $value) {
             //  var_dump($value->ID_admin);
             //  var_dump($value->Password_admin);
@@ -90,7 +54,7 @@ class Member_login extends DQS_controller
 
                 //setcookie("auth",base64_encode($value->mem_username), time() + (86400 * 30), "/"); 
                 //if(isset($_COOKIE["auth"])) {
-                return redirect('Test/test_c/show2');
+                return redirect('Member/v_member_login');
                 // $isLogin = [
                 //     "isAuth" => "success",
 
