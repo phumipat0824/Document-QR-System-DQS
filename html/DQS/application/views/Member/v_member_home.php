247767c11a9de6965b56de0a931c684cf4adfcac
Domeasw <62160115@go.buu.com>
2021-08-16 14:47:33 +0700

    Update v_member_home.php

--- html/DQS/application/views/Member/v_member_home.php
+++ html/DQS/application/views/Member/v_member_home.php
@@ -1,4 +1,4 @@
-<div class="col-lg-4 col-md-4" style="padding-left: 300px; padding-top: 100px; ">
+<div class="col-lg-6 col-md-6" style="padding-left: 350px; padding-top: 100px; ">
     <h1 style="color:#003399">คิวอาร์โค้ดของฉัน</h1>
     <button class="btn btn-primary btn-round" style="background-color: #F5F5F5 ; border: none;">
         <h2 style="color:#003399">+ใหม่</h2>
