82c8dda04027bde234302c15b9bdfbb7d5e6fe3f
Domeasw <62160115@go.buu.com>
2021-08-16 15:47:06 +0700

    Update sidebar_member.php

--- html/DQS/application/views/template/sidebar_member.php
+++ html/DQS/application/views/template/sidebar_member.php
@@ -1,38 +1,40 @@
 <div class="sidebar" data-color="purple" data-background-color="white" data-image="<?php echo base_url().'/assets/img/sidebar-1.jpg'?>">
-      <div class="logo"><a href="http://www.creative-tim.com" class="simple-text logo-normal">
-      Document QR
+    <div class="logo"><a href="http://www.creative-tim.com" class="simple-text logo-normal">
+            Document QR
         </a>
     </div>
-      <div class="sidebar-wrapper ps-container ps-theme-default ps-active-y" data-ps-id="a5787ac4-4df1-fec6-f8e0-33c5f4784969">
+    <div class="sidebar-wrapper ps-container ps-theme-default ps-active-y" data-ps-id="a5787ac4-4df1-fec6-f8e0-33c5f4784969">
         <ul class="nav">
-          <li class="nav-item active  ">
-            <a class="nav-link" href="./dashboard.html">
-              <i class="fas fa-folder"></i>
-              <p>คิวอาร์โค้ดของฉัน</p>
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link" href="./user.html">
-            <i class="fas fa-chart-bar"></i>
-              <p>รายงานสรุปผล</p>
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link" href="./tables.html">
-            <i class="fas fa-cog"></i>
-              <p>จัดการบัญชีผู้ใช้งาน</p>
-            </a>
-          </li>
+            <li class="<?= $_SERVER['REQUEST_URI'] == "/DQS/index.php/Member/Member_login/member_home" ?  ''  : 'active' ?> ">
+                <a class="nav-link" href="<?php echo site_url().'/Member/Member_login/member_home'?>">
+                    <i class="fas fa-folder"></i>
+                    <p>คิวอาร์โค้ดของฉัน</p>
+                </a>
+            </li>
+            <li class="<?= $_SERVER['REQUEST_URI'] == "/DQS/index.php/" ?  'active'  : '' ?> ">
+                <a class="nav-link" href="./user.html">
+                    <i class="fas fa-chart-bar"></i>
+                    <p>รายงานสรุปผล</p>
+                </a>
+            </li>
+            <li class="<?= $_SERVER['REQUEST_URI'] == "/DQS/index.php/" ?  'active'  : '' ?> ">
+                <a class="nav-link" href="./tables.html">
+                    <i class="fas fa-cog"></i>
+                    <p>จัดการบัญชีผู้ใช้งาน</p>
+                </a>
+            </li>
         </ul>
-      <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 0px;">
-        <div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;">
-            </div></div><div class="ps-scrollbar-y-rail" style="top: 0px; right: 0px; height: 333px;">
-                 <div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 214px;">
-                </div>
+        <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 0px;">
+            <div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;">
             </div>
-            <hr style="height:2px;width:70%;border-width:0;color:gray;background-color:gray;margin-left: auto;margin-right: auto;">
         </div>
-        <div class="sidebar-background" style="background-image: url(../assets/img/sidebar-1.jpg) ">
+        <div class="ps-scrollbar-y-rail" style="top: 0px; right: 0px; height: 333px;">
+            <div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 214px;">
+            </div>
+        </div>
+        <hr style="height:2px;width:70%;border-width:0;color:gray;background-color:gray;margin-left: auto;margin-right: auto;">
     </div>
-    
+    <div class="sidebar-background" style="background-image: url(../assets/img/sidebar-1.jpg) ">
+    </div>
+
 </div>
