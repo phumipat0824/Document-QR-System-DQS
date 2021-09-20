a8fb4e5d4ea07ffac63140e5c130af8bc7c13f88
Pongthorn-62160332- <73059406+lnwnice12@users.noreply.github.com>
2021-09-20 17:34:35 +0700

    มอดูลqrcode ผู้ใช่ทั่วไป(เสร็จแล้ว)
    มอดูลqrcode ผู้ใช่ทั่วไป
    -ฟังก์ชั่นสร้าง qrcode

--- html/DQS/application/views/v_qrcode.php
+++ html/DQS/application/views/v_qrcode.php
@@ -1,10 +1,13 @@
+<script type="text/javascript" src="<?php echo base_url() . 'assets/plugin' ?>/qrcode/qrcode.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url() . 'assets/plugin' ?>/qrcode/qrcode.js"></script>
+
 
 <div class="content">
     <div class="content-fluid">
         <div class="row" style="padding: 100px 10px 10px 20%;">       
             <h1>สร้างคิวอาร์โค้ด</h1>
             <h2>เริ่มสร้าง QR Code กันเลย </h2>
-            <div class="col-md-5 ">   
+            <div class="col-md-5"  >   
                 <div class="card card-nav-tabs card-plain">
                     <ul class="nav nav-tabs">
                      <li class="nav-item">
@@ -20,24 +23,28 @@
  
             <div class="tab-pane active" style="padding: 100px 10px 10px 20%;">
             
-                        <form>
+                        
                             <div class="form-row">
-                                <div class="form-group col-md-5">
+                               <div class="form-group col-md-5">
                                 <label >เว็บไซต์</label>
-                                <input placeholder="เว็บไซต์">
+                                <input id="text" type="text"  style="width: 230px;" value=""><br />
+                                
                                 </div>
                                 <br><br>
                                 </div>
                                 <label >โลโก้:</label><br>
-                                <input type="file" name="logoqrcode" required><br><br>
-                                <button type="submit" class="btn btn-dark_blue" style="margin-bottom: 20px;background-color: #100575;color: #fff;">สร้างคิวอาร์โค้ด</button> 
-                            </form>
+                                <input type="file" name="logoqrcode" ><br><br>
+                                <button onclick="makeCode()"class="btn btn-dark_blue" style="margin-bottom: 20px;background-color: #100575;color: #fff;">สร้างคิวอาร์โค้ด</button> 
+                           
                             </div>
                             </div>
                             </div>
               <div class="col-md-5">
-                <div class="card"style="padding-top: 10%">                   
-                <img src="<?php echo base_url(). '/assets/image/QR_home.PNG' ?>"height="50%" width="50%"style="margin: auto;">
+                <div class="card"style="padding-top: 10%" >  
+
+                    <div id="qrcode" style="margin: auto;">
+                    <img src="<?php echo base_url(). '/assets/image/QR_home.PNG' ?>"height="256" width="256"style="margin: auto;">
+                    </div>
                     <div class="card-body" style="margin: auto;">                   
                             
                             <br>
@@ -48,4 +55,37 @@
         </div>
     </div>
 </div>
+
+
+<script type="text/javascript">
+var qrcode = new QRCode(document.getElementById("qrcode"), {
+	width : 50,
+	height : 50
+});
+function makeCode () {		
+	// var elText = document.getElementById("text");
+	
+	// qrcode.makeCode(elText.value);
+    var text = document.getElementById('text');
+    var qrcode = document.getElementById('qrcode');
+
+        if(text.value.trim() !== ''){
+            qrcode.innerHTML = '';
+            new QRCode(qrcode, text.value);
+            
+        }
+}
+
+// makeCode();
+
+// $("#text").
+// 	on("blur", function () {
+// 		makeCode();
+// 	}).
+// 	on("", function (e) {
+// 		if (e.keyCode == 13) {
+// 			makeCode();
+// 		}
+// 	});
+</script>
 
