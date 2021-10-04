d49f6a22c127442508341b351cdede3fef71d2a3
Kiattisak Pumjan <62160075@go.buu.ac.th>
2021-10-05 01:12:33 +0700

    insert and status update department

--- html/DQS/application/models/Da_DQS_department.php
+++ html/DQS/application/models/Da_DQS_department.php
@@ -6,12 +6,46 @@ include_once 'DQS_model.php';
 class Da_DQS_department extends DQS_model
 {
 
-    public $dep_id;
-    public $dep_name;
-    public $dep_active;
-
     public function __construct()
     {
         parent::__construct();
     }
+
+    public function insert()
+	{
+		$sql = "INSERT INTO {$this->db_name}.DQS_Department(dep_name, dep_active) 
+                VALUES (?,?)";
+        $this->db->query($sql, array($this->dep_name,$this->dep_active));
+	}//insert
+
+    
+	
+    public function name_update()
+    {
+        $sql = "UPDATE {$this->db_name}.DQS_Department
+                set dep_name = ? , dep_active = ? 
+                where dep_id = ?";
+
+        $this->db->query($sql , array( $this->dep_name , $this->dep_active , $this->dep_id ) );
+    }
+
+    public function status_update($dep_id,$dep_active)
+    {
+        if ($dep_active == 0) {
+            $sql = "UPDATE {$this->db_name}.DQS_Department
+                set dep_active = 1
+                where dep_id = $dep_id";
+
+            $this->db->query($sql , array( $this->dep_active , $this->dep_id ) );
+        }else if ($dep_active == 1) {
+            $sql = "UPDATE {$this->db_name}.DQS_Department
+                set dep_active = 0
+                where dep_id = $dep_id";
+
+            $this->db->query($sql , array( $this->dep_active , $this->dep_id ) );
+        }
+        
+    }
+
+
 }
--- html/DQS/application/views/template/sidebar_admin.php
+++ html/DQS/application/views/template/sidebar_admin.php
@@ -14,7 +14,7 @@
           <li class="<?= $_SERVER['REQUEST_URI'] == "/DQS/index.php/department/department_list/show_department" ?  'active'  : '' ?> ">
             <a class="nav-link" href="<?php echo site_url().'/department/department_list/show_department' ?>">
             <i class="fas fa-users-cog" style="color: rgb(125, 123, 122);"></i>
-              <p style="color: rgb(0,0,0);">จัดการแผนก</p>
+              <p style="color: rgb(0,0,0);">จัดการหน่วยงาน</p>
             </a>
           </li>
           <li class="<?= $_SERVER['REQUEST_URI'] == "/DQS/index.php/" ?  'active'  : '' ?> ">
