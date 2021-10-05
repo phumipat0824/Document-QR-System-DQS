a4f6f9686bf737eca97ef2e21c0d2e5333c0d390
phumipat0824 <72917897+phumipat0824@users.noreply.github.com>
2021-10-06 00:46:50 +0700

    demo_qrcode

--- html/DQS/application/controllers/Qrcode/QRcode_generator.php
+++ html/DQS/application/controllers/Qrcode/QRcode_generator.php
@@ -12,5 +12,38 @@ class QRcode_generator extends DQS_controller {
 		$this->output("v_qrcode");
 	}
 
+	public function upload_logo(){
+		$dir = "assets/logo/";
+        // $fileImage = $dir . $n_img;
+        $name_type;
+        if(isset($_FILES["logo"]))
+        {           
+                $file_name = $_FILES['logo']['name'];
+                $file_size =$_FILES['logo']['size'];
+                $file_tmp =$_FILES['logo']['tmp_name'];
+                $file_type=$_FILES['logo']['type']; 
+                $n_img = date("Ymd").time().'_'.$file_name;
+                move_uploaded_file($file_tmp,$dir.$n_img);          
+        }
+	}
+
+	public function upload(){
+				/* Get the name of the uploaded file */
+			$filename = $_FILES['logo']['name'];
+			$n_img = date("Ymd").time().'_'.$filename;
+			$this->session->set_userdata('logo_name', $n_img);
+			/* Choose where to save the uploaded file */
+			$location = "assets/logo/".$n_img;
+
+			/* Save the uploaded file to the local filesystem */
+			if ( move_uploaded_file($_FILES['logo']['tmp_name'], $location) ) { 
+			echo 'Success'; 
+			} else { 
+			echo 'Failure'; 
+			}
+
+		}
+
+
 }
 ?>
--- html/DQS/application/views/v_qrcode.php
+++ html/DQS/application/views/v_qrcode.php
@@ -1,24 +1,14 @@
 
 <script src="<?php echo base_url() . 'node_modules/easyqrcodejs/src' ?>/easy.qrcode.js"></script>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
+
 
 <div class="content">
-    <div class="content-fluid">
         <div class="row" style="padding: 100px 10px 10px 20%;">       
             <h1 style="color:#100575; font-family:TH sarabun new; font-size: 80px;" >สร้างคิวอาร์โค้ด</h1>
             <h2 style="font-family:TH sarabun new; ">เริ่มสร้าง QR Code กันเลย </h2>
             <div class="col-md-5"  >   
-                <div class="card card-nav-tabs card-plain">
+                <div class="card card-nav-tabs card-plain"style="color: #E0FFFF">
                     <ul class="nav nav-tabs">
                      <li class="nav-item">
                         <a class="nav-link active" href="#">เว็บไซต์</a>
@@ -30,69 +20,114 @@
                         <a class="nav-link" href="#">รูปภาพ</a>
                      </li>
                     </ul>
- 
-            <div class="tab-pane active" style="padding: 10%; margin: auto; ">
-            
-                        
+                    <div class="card-body" style="margin: auto;margin-top: 80px;">
                             <div class="form-row">
-                            <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-    </div>
-                               <div class="form-group col-md-5">
+                              <div class="form-group col-md-5">
                                 <label >เว็บไซต์</label>
                                 <input id="text" type="text"  style="width: 230px;" value=""><br />
                                 
                                 </div>
                                 <br><br>
                                 </div>
-                                <label >โลโก้:</label><br>
-                                <input id="logo" type="file" name="logo" ><br><br>
-                                <button onclick="make()"class="btn btn-dark_blue" style="margin-bottom: 20px;background-color: #100575;color: #fff;">สร้างคิวอาร์โค้ด</button> 
-                           
+                               <!-- <label >โลโก้:</label><br>
+                                <input id="logo" type="file" name="logo" onchange="uploadFile()"><br><br>
+                                <input id="logoinqr" type="text" name="logoinqr" value="<?php echo $this->session->userdata('logo_name')?>" hidden > -->
+                                <button onclick="make()" class="btn btn-dark_blue" style="margin-left: 10px;margin-bottom: 50px;margin-top:100px;background-color: #100575;color: #fff; width: 240;font-family:TH sarabun new; font-size: 20px;">สร้างคิวอาร์โค้ด</button> 
+                                
+                            
+                                </div>
                             </div>
                             </div>
-                            </div>
-              <div class="col-md-5">
-                <div class="card"style="padding: 10%" >  
 
-                    
-                    <!-- <img src="<?php echo base_url(). '/assets/image/QR_home.PNG' ?>"height="256" width="256"style="margin: auto;"> -->
-                    
-                    <div class="card-body" style="margin: auto;">                   
-                          <div id="qrcode">  </div>
+                         <div class="col-md-5">
+                         <div class="card"style="padding: 10%" height="500" >                      
+                                <div class="card-body" style="margin: auto;">                   
+                          <div id="qrcode">
+                          <img id="img"src="<?php echo base_url(). '/assets/image/QR_home.PNG' ?>"height="256" width="256"style="margin: auto;">
+                          </div>
                             <br>
-                            <button type="submit" class="btn btn-warning" style="font-family:TH sarabun new; font-size: 30px; width: 243; height:48 ;">ดาวน์โหลด</button> 
+                            <button type="submit" class="btn btn-warning" style="font-family:TH sarabun new; font-size: 20px; width: 240; ">ดาวน์โหลด</button> 
                     </div>
                 </div>
              </div>
         </div>
-    </div>
 </div>
-</body>
-</html>
+<!-- </form> -->
 
-<script>
+<script type="text/javascript">
 function make() {		
     var text = document.getElementById('text');
     var qrcode = document.getElementById('qrcode');
-    var logo = document.getElementById('logo');
-
+    var logo = "<?php echo base_url(). '/assets/logo' ?>"+ document.getElementById('logoinqr');
+   
+    
         if(text.value.trim() !== ''){
             qrcode.innerHTML = '';
-            new QRCode(qrcode, text.value ,logo.value);
+            new QRCode(qrcode, text.value);
             
         }
 }
 
-var qrcode = new QRCode(document.getElementById("qrcode"), {
-    text: "https://cssscript.com",
-    logo: "<?php echo base_url(). '/assets/image/logo_dqs.png' ?>",
-    logoWidth: undefined,
-    logoHeight: undefined,
-    logoBackgroundColor: '#ffffff',
-    logoBackgroundTransparent: false
-});
+// var qrcode = new QRCode(document.getElementById("qrcode"), {
+//     text: "https://cssscript.com",
+//     //logo: "<?php echo base_url(). '/assets/image/logo_dqs.png' ?>",
+//     logoWidth: undefined,
+//     logoHeight: undefined,
+//     logoBackgroundColor: '#ffffff',
+//     logoBackgroundTransparent: false
+// });
+
+
+function upload_logo(logo) {
+$.ajax({
+    type: "POST",
+    url: "<?php echo site_url() . "/Qrcode/QRcode_generator/upload_logo/" ?>",
+    data: {
+        'logo': logo,
+    },
+    dataType: 'JSON',
+    async: false,
+    cache : false,
+    processData: false,
+    success: function(jsondata) {
+         alert(logo)
+    }
+})
+
+}
+
+async function uploadFile() {
+  let formData = new FormData(); 
+  formData.append("logo", logo.files[0]);
+  await fetch( "<?php echo site_url() . "/Qrcode/QRcode_generator/upload/" ?>", {
+    method: "POST", 
+    body: formData
+  }); 
+  alert('The file has been uploaded successfully.');
+  }
+
+
 
 </script>
+<style>
+
+.nav-tabs .nav-item .nav-link, .nav-tabs .nav-item .nav-link:focus, .nav-tabs .nav-item .nav-link:hover {
+    border: 0!important;
+    color: #000!important;
+    font-weight: 500;
+}
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+#img {
+  -webkit-filter: blur(2px); /* Safari 6.0 - 9.0 */
+  filter: blur(2px);
+}
+</style>
 
