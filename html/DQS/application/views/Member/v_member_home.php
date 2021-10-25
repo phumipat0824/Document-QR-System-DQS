91b566fdb3bc76dc9da14454fefc2a4d16009426
Domeasw <73055457+Domeasw@users.noreply.github.com>
2021-10-25 15:34:24 +0700

    Update v_member_home.php

--- html/DQS/application/views/Member/v_member_home.php
+++ html/DQS/application/views/Member/v_member_home.php
@@ -1,7 +1,19 @@
-<div class="col-lg-6 col-md-6" style="padding-left: 350px; padding-top: 100px; ">
+<div class="col-md-6" style="padding-left: 350px; padding-top: 100px; ">
     <h1 style="color:#003399">คิวอาร์โค้ดของฉัน</h1>
     <button class="btn btn-primary btn-round" style="background-color: #F5F5F5 ; border: none;">
         <h2 style="color:#003399">+ใหม่</h2>
     </button>
-    </button>
+
+</div>
+
+<div class="col-md-6" style="padding-left: 350px; padding-top: 100px; border-radius: 10px;">
+    <div class="card">
+        <div class="card-header-" style="border-radius: 10px; background-color: #100575; text-align:center;">
+            <h style="color:#FFFFFF; font-family:TH Sarabun New; font-size: 40px; ">คิวอาร์โค้ด</h>
+        </div>
+        <div class="card-body">
+            .....
+        </div>
+    </div>
+</div>
 </div>
