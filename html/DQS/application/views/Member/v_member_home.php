44b22d7b3a12f4ac3865e6d45abeeadedc7dfd50
62160147 <73161408+62160147@users.noreply.github.com>
2021-10-25 15:44:16 +0700

    v_upload_file

--- html/DQS/application/views/Member/v_member_home.php
+++ html/DQS/application/views/Member/v_member_home.php
@@ -1,7 +1,25 @@
 <div class="col-lg-6 col-md-6" style="padding-left: 350px; padding-top: 100px; ">
     <h1 style="color:#003399">คิวอาร์โค้ดของฉัน</h1>
     <button class="btn btn-primary btn-round" style="background-color: #F5F5F5 ; border: none;">
-        <h2 style="color:#003399">+ใหม่</h2>
+        <h2 class="right" style="color:#003399"  >+สร้าง</h2>   
     </button>
-    </button>
+</div>
+<div class="col-md-5" style="padding-left: 350px; padding-top: 100px; border-radius: 10px;">
+    <div class="card">
+        <div class="card-header-" style="border-radius: 10px; background-color: #100575; text-align:center;">
+            <h style="color:#FFFFFF; font-family:TH Sarabun New; font-size: 40px; ">คิวอาร์โค้ด</h>
+        </div>
+        <div class="card-body">
+            <h2>ชื่อ</h2><br>
+
+            <h2>วันที่สร้าง</h2><br>
+
+            <h2>ชนิด</h2>
+            <button type="submit" > ดาวน์โหลด</button>
+            <button type="submit" > แก้ไข้</button>
+            <button type="submit" > ย้าย</button>
+            <button type="submit" > ลบ</button>
+        </div>
+    </div>
+</div>
 </div>
