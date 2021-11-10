3d69dc5271fce9ffc5a505ce8993aa68ef78f939
nniddnoy <73216249+nniddnoy@users.noreply.github.com>
2021-11-10 14:56:38 +0700

    v_member_upload_file.php

---
+++ html/DQS/application/controllers/Member/Member_upload_file.php
@@ -0,0 +1,46 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require dirname(__FILE__).'/../DQS_controller.php';
+
+class Member_upload_file extends DQS_controller {
+
+	public function show_member_upload_file()
+	{
+
+		$this->output("v_qrcode");
+	}
+
+	public function upload(){
+			$this->session->unset_userdata('logo_name');
+				/* Get the name of the uploaded file */
+			$filename = $_FILES['logo']['name'];
+			$n_img = date("Ymd").time().'_'.$filename;
+			$this->session->set_userdata('logo_name', $n_img);
+			/* Choose where to save the uploaded file */
+			$location = "assets/logo/".$n_img;
+
+			/* Save the uploaded file to the local filesystem */
+			if ( move_uploaded_file($_FILES['logo']['tmp_name'], $location) ) { 
+			echo 'Success'; 
+			} else { 
+			echo 'Failure'; 
+			}
+
+		}
+		public function dowload(){
+		if (isset($_GET['file'])) {
+			$file = $_GET['file'];
+			if (file_exists($file) && is_readable($file) && preg_match('/\.png$/',$file)) {
+				header('Content-Type: application/png');
+				header("Content-Disposition: attachment; filename=\"$file\"");
+				readfile($file);
+				}
+			}
+
+		}
+
+
+}
+?>
