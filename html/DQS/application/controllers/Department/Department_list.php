52d59acd9087bfe57228b6d0fc31a77bd672810d
Kiattisak Pumjan <62160075@go.buu.ac.th>
2021-11-13 14:48:05 +0700

    delete comment code

--- html/DQS/application/controllers/Department/Department_list.php
+++ html/DQS/application/controllers/Department/Department_list.php
@@ -8,9 +8,7 @@ class Department_list extends DQS_controller {
 
 	public function index()
 	{
-		// $this->load->model('M_DQS_department', 'dept');
-		// $pudata['arr_dept'] = $this->dept->get_all()->result();
-		// $this->output_sidebar_admin('department/v_department');
+		
 	}
 
 	public function show_department()
@@ -35,14 +33,7 @@ class Department_list extends DQS_controller {
 			$this->MDD->insert();
 			redirect('/department/department_list/show_department');
 		}
-		// else{
-		// 	echo "
-		// 	<script>
-		// 		alert('ข้อมูลซ้ำในระบบหรือไม่กรอกข้อมูล กรุณากรอกใหม่');
-		// 		window.location.href='show_department';
-		// 	</script>";
-			
-		// }
+		
     }
 
 	public function edit_department()
@@ -55,22 +46,9 @@ class Department_list extends DQS_controller {
 			$this->MDD->name_update();
 			redirect('/department/department_list/show_department');
 		}
-		// else{
-		// 	echo "
-		// 	<script>
-		// 		alert('ข้อมูลซ้ำในระบบหรือไม่กรอกข้อมูล กรุณากรอกใหม่');
-		// 		window.location.href='show_department';
-		// 	</script>";
-			
-		// }
+		
     }
 	
-	// public function check_dept_name(){
-	// 	$this->load->model('M_DQS_department', 'MDD');
-	// 	$this->MDD->dep_name = $this->input->post('dep_name');
-    //     $data['json_check_dept'] = $this->dept->check_exist_name($this->MDD->dep_name)->row();
-    //     echo json_encode($data);
-	// }
 	
 	public function update_status(){
 		$this->load->model('M_DQS_department', 'MDD');
