7bbe8d7b7e60400a8e2c6e66d7b5d8c54e0aa51c
Pongthorn-62160332- <73059406+lnwnice12@users.noreply.github.com>
2021-11-20 04:53:25 +0700

    คลิกขวาตรงสร้าง จะมีตัวเลือก สร้างโฟลเดอร์กับอัปโหลดไฟล์

--- html/DQS/application/views/Member/v_member_home.php
+++ html/DQS/application/views/Member/v_member_home.php
@@ -4,6 +4,7 @@
     <h1 style="color:#003399; font-family:TH Sarabun New; font-weight: 900; font-size: 80px;" >คิวอาร์โค้ดของฉัน</h1>
     </div>
     <div class="col-md-4">
+    
     <button class="btn btn-primary btn-round" style=" width: 145px; background-color: #F5F5F5 ; border: none;" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
     <h1 style="font-weight: 900; color:#003399 ; font-size: 50px; font-family:TH Sarabun New; height: 40; width: 50px;" id="button-folder" >+ สร้าง</h1>
     </button>
@@ -12,15 +13,19 @@
 
 <h3 style="color:#707070; font-family:TH Sarabun New; font-weight: 900;" >โฟลเดอร์</h3>
     
+<div class="custom-cm">
+  <div class="custom-cm__item" data-toggle="modal" data-target="#exampleModal">สร้างโฟลเดอร์</div>
+  <div class="custom-cm__divider"></div>
+  <div  class="custom-cm__item" ><a href="<?php echo site_url() . '/Qrcode/QRcode_generator/show_qrcode'; ?>">อัปโหลดไฟล์</a></div> 
+</div>
 <div class="row">
 <?php 
     for ($i = 0; $i < count($arr_fol); $i++) {   ?>   
         
-    
             <div class="col-3">        
-                    <div class="card card-frame" style=" height: 60px; width: 260px;">
+            <div class="card card-frame" style=" height: 60px; width: 260px;">
                             <div class="card-body"> 
-                                <p style="font-size: 26px; font-family:TH Sarabun New;"> 
+                                <p style="font-size: 26px;  font-family:TH Sarabun New;"> 
                                 <i class="material-icons">folder</i> 
                                 <?php echo $arr_fol[$i]->fol_name ?>
                                 </p>
@@ -55,6 +60,65 @@
             </form>
     </div>  
 </div>
+<style>
+    a{
+        text-decoration: none;
+            color: black;
+    }
+    a:hover{
+        text-decoration: none;
+            color: black;
+    }
+    .custom-cm {
+  background-color: #ffffff;
+  border: 1px solid #cccccc;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  display: none;
+  padding: 10px 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 200px;
+}
 
-  
+.custom-cm__item {
+  cursor: pointer;
+  padding: 8px 15px;
+}
 
+.custom-cm__item:hover {
+  background-color: #f8f8f8;
+}
+
+.custom-cm__divider {
+  border-bottom: 1px solid #eeeeee;
+  margin: 10px 0;
+}
+
+</style>
+<script>
+    const cm = document.querySelector(".custom-cm");
+
+function showContextMenu(show = true) {
+  cm.style.display = show ? "block" : "none";
+}
+
+window.addEventListener("contextmenu", e => {
+  e.preventDefault();
+
+  showContextMenu();
+  cm.style.top =
+    e.y + cm.offsetHeight > window.innerHeight
+      ? window.innerHeight - cm.offsetHeight
+      : e.y;
+  cm.style.left =
+    e.x + cm.offsetWidth > window.innerWidth
+      ? window.innerWidth - cm.offsetWidth
+      : e.x;
+});
+
+window.addEventListener("click", () => {
+  showContextMenu(false);
+});
+
+</script>
