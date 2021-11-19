6803eee3ad98cd77101f6dd116a78016ae9a1478
icekys26 <62160152@go.buu.ac.th>
2021-11-19 22:23:55 +0700

    หน้าจอจัดการบัญชีผู้ใช้งานของสมาชิก
    หน้าจอการเปลี่ยนรหัสผ่าน

--- html/DQS/application/views/template/sidebar_member.php
+++ html/DQS/application/views/template/sidebar_member.php
@@ -26,8 +26,8 @@
                     <p style="color: rgb(0,0,0); ">รายงานสรุปผล</p>
                 </a>
             </li>
-            <li class="<?= $_SERVER['REQUEST_URI'] == "/DQS/index.php/" ?  'active'  : '' ?> ">
-                <a class="nav-link" href="./tables.html">
+            <li class="<?= $_SERVER['REQUEST_URI'] == "/DQS/index.php/Member/Member_changepass/show_changepass" ?  'active'  : '' ?> ">
+                <a class="nav-link" href="<?php echo site_url().'/Member/Member_changepass/show_changepass'?>">
                     <i class="fas fa-cog" style="color: rgb(125, 123, 122);"></i>
                     <p style="color: rgb(0,0,0); ">จัดการบัญชีผู้ใช้งาน</p>
                 </a>
