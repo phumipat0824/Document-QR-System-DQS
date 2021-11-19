357b3c372d8c5d65b0d707c14501190bfc16b515
Domeasw <73055457+Domeasw@users.noreply.github.com>
2021-11-19 20:09:44 +0700

    Update M_DQS_qrcode.php

---
+++ html/DQS/application/models/M_DQS_qrcode.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+include_once 'Da_DQS_qrcode.php';
+
+class M_DQS_qrcode extends Da_DQS_qrcode
+{
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
