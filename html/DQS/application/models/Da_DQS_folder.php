97abdc79827702c57379a3f2f1679a7d5ba5459a
62160343 <62160343@go.buu.ac.th>
2021-12-03 20:36:08 +0700

    Update Da_DQS_folder.php

--- html/DQS/application/models/Da_DQS_folder.php
+++ html/DQS/application/models/Da_DQS_folder.php
@@ -42,6 +42,7 @@ class Da_DQS_folder extends DQS_model {
     }//end insert folder into database
 
     public function update(){//update folder into database    
+        $sql ="UPDATE {$this->db_name}.DQS_Folder
         SET fol_name = ?
         WHERE fol_id = ?";
         $this->db->query($sql,array($this->fol_name,$this->fol_id));         
@@ -52,7 +53,7 @@ class Da_DQS_folder extends DQS_model {
         $sql ="DELETE FROM {$this->db_name}.DQS_Folder
         WHERE fol_id = $fol_id";
         $this->db->query($sql , array($this->fol_id) );  
-
 	}
 
 }
+?>
