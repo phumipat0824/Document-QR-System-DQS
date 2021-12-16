a3074f755315ce360ccea161c07763e5f3c00d72
Domeasw <73055457+Domeasw@users.noreply.github.com>
2021-12-16 18:03:12 +0700

    Update Member_upload_file.php

--- html/DQS/application/controllers/Member/Member_upload_file.php
+++ html/DQS/application/controllers/Member/Member_upload_file.php
@@ -18,22 +18,32 @@ class Member_upload_file extends DQS_controller {
 		$this->output("member/test_upload");
 	}
 		public function upload(){
-			//$this->session->unset_userdata('logo_name');
-				/* Get the name of the uploaded file */
-			$filename = $_FILES['logo']['name'];
-			$n_img = date("Ymd").time().'_'.$filename;
-			$this->session->set_userdata('logo_name', $n_img);
-			/* Choose where to save the uploaded file */
-			$location = "assets/logo/".$n_img;
-
-			/* Save the uploaded file to the local filesystem */
-			if ( move_uploaded_file($_FILES['logo']['tmp_name'], $location) ) { 
-			echo 'Success'; 
-			} else { 
-			echo 'Failure'; 
+			
+			$this->load->model('Da_DQS_qrcode','dqrc');
+			$this->dqrc->doc_type = "pdf";
+			$this->dqrc->doc_name = "Dome";
+			$upload=$_FILES['doc_path'];
+			if($upload != '') {   //not select file
+			//โฟลเดอร์ที่จะ upload file เข้าไป 
+				$path= dirname(__FILE__).'/../../../assets/pdf/fileupload_Member/';  
+			
+			//เอาชื่อไฟล์เก่าออกให้เหลือแต่นามสกุล
+				$type = strrchr($_FILES['doc_path']['name'],".");
+				
+			//ตั้งชื่อไฟล์ใหม่โดยเอาเวลาไว้หน้าชื่อไฟล์เดิม
+				$newname = "Dome".$type;
+				$path_copy=$path.$newname;
+				
+				$newpath = '/assets/pdf/fileupload_Member/'.$newname;
+			//คัดลอกไฟล์ไปเก็บที่เว็บเซริ์ฟเวอร์
+				move_uploaded_file($_FILES['doc_path']['tmp_name'],$path_copy);  	
+			}//if
+			$this->session->set_userdata('newpath', $newpath);
+			$this->dqrc->doc_path = $newpath;
+			$this->dqrc->insert();
+			
 		}
 
-		}
 		public function save_qrcode(){
 					// Get the incoming image data
 			$image = $_POST["image"];
