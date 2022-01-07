b3d4eac762562df24adc4d77374605a48c6ac8ec
phumipat0824 <72917897+phumipat0824@users.noreply.github.com>
2022-01-07 20:35:48 +0700

    แก้ไข QR code ของผู้ใช้งานทั่วไป

--- html/DQS/application/views/v_qrcode.php
+++ html/DQS/application/views/v_qrcode.php
@@ -3,6 +3,7 @@
         <a style="color:#100575; font-size: 80px;">สร้างคิวอาร์โค้ด</a>
         <a style="font-size: 35px;">เริ่มสร้าง QR Code กันเลย </a>
     </div>
+    
     <div class="row"style="padding-left:20%;">
             <div class="col-md-5">
                 <div class="card"style="border-color:#E4E4E4;border-width: 5px;"> 
@@ -14,39 +15,31 @@
                 </div>
             </nav>                                         
                     <!-- <form action='' method="post"> -->
-                    <div class="form-row"style="margin-left:15%;margin-top: 10%;">                
+                    <div class="form-row"style="margin-left:10%;margin-top: 10%;margin-bottom: 20px">                
                             <a style="margin-top: 10px;">เว็บไซต์</a>
-                            <label style = "color: #FF0000;">*</label><br>
+                            <label style = "color: #FF0000;">*</label>
                     </div>
-                    <div class="form-row"style="margin: auto;">
-                    <div class="col-25"> 
-                            <input id="text" type="text"placeholder="http://"oninvalid="InvalidMsg(this);"oninput="InvalidMsg(this);"value="https://www.example.com"required="required">
-                    </div>
-                    </div>
-                    <div class="form-row"style="margin: auto;"> 
+
+                            <input id="text" type="text"style="margin: auto;" placeholder="http://"oninvalid="InvalidMsg(this);"oninput="InvalidMsg(this);"value="https://www.example.com"required="required">
                                 <div id="inputlogo"style="margin-top: 30px;">
-                                    <button class = "slide" style="width: 100%;" onclick="showinputlogo()">เพิ่มโลโก้</button>                               
-                                </div>
-                    </div>    
-                                        <div id="myDIV"style="display:none;margin-top: 30px;">
-                                        <div class="form-row"style="margin-left:65px;margin-top: 50px;"> 
-                                        <a style="margin-top: 10px;">โลโก้</a>
-                                            <label>     jpg/png</label><br>
+                                <center>
+                                    <button class = "slide" id="up" style="width: 80%;" onclick="showinputlogo()">เพิ่มโลโก้<i class="fas fa-angle-down" style="float: right;"  aria-hidden="true"></i></button>
+                                </center>                               
+                                </div> 
+                                        
+                                        <div class="form-row" id="vanish"style="display:none;margin-left:10%;margin-top: 30px;"> 
+                                        <a style="margin-top: 10px;">โลโก้</a>&emsp;
+                                            <label>     jpg/png</label>
                                         </div> 
-                                                <div class="parent-div">
-                                                <div class="form-row"style="margin: auto;">
-                                                    <button class="btn-upload" style="color:#cfcfcf;"><i class="fas fa-upload"></i> เลือกโลโก้คิวอาร์โค้ด</button>                   
-                                                    <input id="logo_img" type="file" name="logo" accept="image/png, image/jpeg"><br><br>
-                                                </div>
-                                                </div>
-                                        </div> 
+                                        <div class="parent-div" id="vanish1"style="display:none;margin-left:10%;">      
+                                                <button class="btn-upload" style="color:#cfcfcf;"><i class="fas fa-upload"></i> เลือกโลโก้คิวอาร์โค้ด</button>                   
+                                                <input id="logo_img" type="file" name="logo" accept="image/png, image/jpeg"><br><br>
+                                        </div>        
                                         <div class="form-row"style="margin: auto;">                                      
                                         <button type="submit"  onclick="make()" class="btn btn-dark_blue" style="margin-bottom: 30px;margin-top: 40px;background-color: #100575;color: #fff; width: 240;font-family:TH sarabun new; font-size: 35px;">สร้างคิวอาร์โค้ด</button>
-                                        </div>     
-                    
+                                        </div>                                
                     </div>
-                    </div>
-                    
+                    </div>                   
             <div class="col-md-5">
                 <div class="card"style="border-color:#E4E4E4;border-width: 5px;">
                         <center>
@@ -55,15 +48,189 @@
                                     <img id="img" src="<?php echo base_url(). '/assets/image/QR_home.PNG' ?>" height="250" width="250" style="margin: auto;">                       
                                 </div> 
                             </div> 
-                            <button id="download" onclick="doCapture();" class="btn btn-warning" style="margin-top:40px;margin-bottom: 30px;font-family:TH sarabun new; font-size: 35px; width: 240; ">ดาวน์โหลด</button> 
+                            <button id="download" class="btn btn-warning" style="margin-top:40px;margin-bottom: 30px;font-family:TH sarabun new; font-size: 35px; width: 240; ">ดาวน์โหลด</button> 
                         </center>
                 </div>
             </div>
     </div>
+
+    <div class="stage" id="tour-edit-save">
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+</div>
+<style>
+.stage {
+  height: 300px;
+  width: 500px;
+  margin: auto;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  perspective: 9999px;
+  transform-style: preserve-3d;
+}
+
+.layer {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  transform-style: preserve-3d;
+  animation: ಠ_ಠ 5s infinite alternate ease-in-out -7.5s;
+  animation-fill-mode: forwards;
+  transform: rotateY(40deg) rotateX(33deg) translateZ(0);
+}
+
+.layer:after {
+  font: 150px/0.65 "Pacifico", "Kaushan Script", Futura, "Roboto", "Trebuchet MS", Helvetica, sans-serif;
+  content: "Phumipat\a    Katsuwan!";
+  white-space: pre;
+  text-align: center;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 50px;
+  color: whitesmoke;
+  letter-spacing: -2px;
+  text-shadow: 4px 0 10px rgba(0, 0, 0, 0.13);
+}
+
+.layer:nth-child(1):after {
+  transform: translateZ(0px);
+}
+
+.layer:nth-child(2):after {
+  transform: translateZ(-1.5px);
+}
+
+.layer:nth-child(3):after {
+  transform: translateZ(-3px);
+}
+
+.layer:nth-child(4):after {
+  transform: translateZ(-4.5px);
+}
+
+.layer:nth-child(5):after {
+  transform: translateZ(-6px);
+}
+
+.layer:nth-child(6):after {
+  transform: translateZ(-7.5px);
+}
+
+.layer:nth-child(7):after {
+  transform: translateZ(-9px);
+}
+
+.layer:nth-child(8):after {
+  transform: translateZ(-10.5px);
+}
+
+.layer:nth-child(9):after {
+  transform: translateZ(-12px);
+}
+
+.layer:nth-child(10):after {
+  transform: translateZ(-13.5px);
+}
+
+.layer:nth-child(11):after {
+  transform: translateZ(-15px);
+}
+
+.layer:nth-child(12):after {
+  transform: translateZ(-16.5px);
+}
+
+.layer:nth-child(13):after {
+  transform: translateZ(-18px);
+}
+
+.layer:nth-child(14):after {
+  transform: translateZ(-19.5px);
+}
+
+.layer:nth-child(15):after {
+  transform: translateZ(-21px);
+}
+
+.layer:nth-child(16):after {
+  transform: translateZ(-22.5px);
+}
+
+.layer:nth-child(17):after {
+  transform: translateZ(-24px);
+}
+
+.layer:nth-child(18):after {
+  transform: translateZ(-25.5px);
+}
+
+.layer:nth-child(19):after {
+  transform: translateZ(-27px);
+}
+
+.layer:nth-child(20):after {
+  transform: translateZ(-28.5px);
+}
+
+.layer:nth-child(n+10):after {
+  -webkit-text-stroke: 3px rgba(0, 0, 0, 0.25);
+}
+
+.layer:nth-child(n+11):after {
+  -webkit-text-stroke: 15px dodgerblue;
+  text-shadow: 6px 0 6px #00366b, 5px 5px 5px #002951, 0 6px 6px #00366b;
+}
+
+.layer:nth-child(n+12):after {
+  -webkit-text-stroke: 15px #0077ea;
+}
+
+.layer:last-child:after {
+  -webkit-text-stroke: 17px rgba(0, 0, 0, 0.1);
+}
+
+.layer:first-child:after {
+  color: #fff;
+  text-shadow: none;
+}
+
+@keyframes ಠ_ಠ {
+  100% {
+    transform: rotateY(-40deg) rotateX(-43deg);
+  }
+}
+</style>
 </div>
 
 <script type="text/javascript">
-    
+  $('#tour-edit-save').removeAttr('onclick');
+  $(document).ready(function() {
+  $(document).on('click', '.stage', function() {
+    $(this).remove('.stage');
+  });
+});   
 async function uploadFile() {
     let formData = new FormData();
     formData.append("logo", logo.files[0]);
@@ -73,6 +240,7 @@ async function uploadFile() {
     });
     // alert('The file has been uploaded successfully.');
 }
+
 function make() {
   const [file] = logo_img.files
   if (file) {
@@ -160,11 +328,15 @@ function InvalidMsg(textbox) {
 }
 
 function showinputlogo() { 
-    var x = document.getElementById("myDIV");
+    $('#up').find("i").toggleClass("fa-angle-down fa-angle-up");
+    var x = document.getElementById("vanish");
+    var y = document.getElementById("vanish1");
         if (x.style.display === "none") {
             x.style.display = "block";
+            y.style.display = "block";
         } else {
             x.style.display = "none";
+            y.style.display = "none";
         }
         
 }
@@ -199,6 +371,9 @@ function doCapture() {
     });
 }
 
+        
+  
+
 
 </script>
 <style>
@@ -214,7 +389,7 @@ function doCapture() {
 
 input[type=text],
 select {
-    width: 100%;
+    width: 80%;
     padding: 12px 20px;
     margin: 8px 0;
     display: inline-block;
@@ -229,6 +404,7 @@ select {
   display: inline-block;
   position: relative;
   overflow: hidden;
+  /* margin: auto; */
 }
 .parent-div input[type=file] {
   left: 0;
@@ -238,10 +414,10 @@ select {
   font-size: 90px;
 }
 .btn-upload {
-    width: 100%;
+    width: 89%;
     height : 47px;
     padding: 12px 20px;
-    margin: 8px 0;
+    margin: auto;
     display: inline-block;
     border: 1px solid #ccc;
     border-radius: 4px;
@@ -253,11 +429,12 @@ select {
     /* Safari 6.0 - 9.0 */
     filter: blur(2px);
 }
+
 a{
     font-size: 16px
 }
 .slide{
-    width: 100%;
+    width: 80%;
     padding: 12px 20px;
     margin: 8px 0;
     display: inline-block;
@@ -276,4 +453,5 @@ a{
 .nav-tabs .nav-link.active:focus, .nav-tabs .nav-link.active:hover {
     border-color: #fff;
 }
+
 </style>
