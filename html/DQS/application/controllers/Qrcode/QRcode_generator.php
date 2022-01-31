8bf72e1a72bdcc93aa6516001343094a62f639d4
phumipat0824 <72917897+phumipat0824@users.noreply.github.com>
2022-01-31 20:46:14 +0700

    change the session module's

--- html/DQS/application/controllers/Qrcode/QRcode_generator.php
+++ html/DQS/application/controllers/Qrcode/QRcode_generator.php
@@ -1,65 +1,18 @@
+<!-- /*
+* Qrcode_generator
+* show_qrcode
+* @author Phumipat 
+* @Create Date 2564-08-05
+*/ -->
 <?php
-
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 require dirname(__FILE__).'/../DQS_controller.php';
 
-class QRcode_generator extends DQS_controller {
+class Qrcode_generator extends DQS_controller {
 
-	public function show_qrcode()
-	{
-		//$this->session->unset_userdata('logo_name');
-		$this->output("v_qrcode");
+	public function show_qrcode(){
+		$this->output("Qrcode/v_qrcode");
 	}
-
-		public function save_qrcode(){
-					// Get the incoming image data
-		$image = $_POST["image"];
-		
-		// Remove image/jpeg from left side of image data
-		// and get the remaining part
-		$image = explode(";", $image)[1];
-		
-		// Remove base64 from left side of image data
-		// and get the remaining part
-		$image = explode(",", $image)[1];
-		
-		// Replace all spaces with plus sign (helpful for larger images)
-		$image = str_replace(" ", "+", $image);
-		
-		// Convert back from base64
-		$image = base64_decode($image);
-		
-		// Save the image as filename.jpeg
-		file_put_contents("filename.jpeg", $image);
-		
-		// Sending response back to client
-		echo "Done";
-				}
-	public function save_server(){			
-							// Get the incoming image data
-			$image = $_POST["image"];
-			
-			// Remove image/jpeg from left side of image data
-			// and get the remaining part
-			$image = explode(";", $image)[1];
-			
-			// Remove base64 from left side of image data
-			// and get the remaining part
-			$image = explode(",", $image)[1];
-			
-			// Replace all spaces with plus sign (helpful for larger images)
-			$image = str_replace(" ", "+", $image);
-			
-			// Convert back from base64
-			$image = base64_decode($image);
-			
-			// Save the image as filename.jpeg
-			file_put_contents(dirname(__FILE__) . '/../../../assets/logo/filename.jpeg', $image);
-			
-			// Sending response back to client
-			echo "Done";
-	}
-
 }
 ?>
