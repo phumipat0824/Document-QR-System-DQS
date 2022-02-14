f25f6a3e58ddfca1ad692ad42cc74ff4ac4657c1
Domeasw <73055457+Domeasw@users.noreply.github.com>
2022-02-14 18:51:08 +0700

    Update_member_upload_file

--- html/DQS/application/models/M_DQS_qrcode.php
+++ html/DQS/application/models/M_DQS_qrcode.php
@@ -3,6 +3,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 include_once 'Da_DQS_qrcode.php';
 
+/*
+* M_DQS_qrcode
+* @author Ashirawat
+* @Create Date 2564-11-14
+*/
+
 class M_DQS_qrcode extends Da_DQS_qrcode
 {
 
