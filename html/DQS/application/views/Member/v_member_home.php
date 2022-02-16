e2f8458ba32926de980ad4a550cc94b183990e07
lnwnice12 <lnwnice12@gmail.com>
2022-02-16 13:42:01 +0700

    อัพเดทหน้า view
    อัพเดทหน้า view

--- html/DQS/application/views/Member/v_member_home.php
+++ html/DQS/application/views/Member/v_member_home.php
@@ -15,14 +15,14 @@
             <h1 style="color:#003399; font-family:TH Sarabun New; font-weight: 900; font-size: 80px;">คิวอาร์โค้ดของฉัน</h1>
         </div>
         <div class="col-md-4">
-            <div class="dropdown">
+            <div class="dropdown" >
                 <button onmousedown="rightclick()" class="dropbtn btn btn-round" style=" width: 145px; background-color: #F5F5F5 ; border: none;">
                     <h1 style="font-weight: 900; color:#003399 ; font-size: 50px; font-family:TH Sarabun New; height: 40; width: 50px;" id="button-folder">+ สร้าง</h1>
                 </button>
                 <div id="myDropdown" class="dropdown-content">
                     <div class="custom-cm__item" data-toggle="modal" data-target="#exampleModal"><a>สร้างโฟลเดอร์</a></div>
 
-                    <div class="custom-cm__item"><a href="<?php echo site_url() . '/Member/Member_upload_file/show_member_upload_file'; ?>">อัปโหลดไฟล์</a></div>
+                    <div  class="custom-cm__item"><a href="<?php echo site_url() . '/Member/Member_upload_file/show_member_upload_file'; ?>">อัปโหลดไฟล์</a></div>
                 </div>
             </div>
         </div>
