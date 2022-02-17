d1c29860cf82b68a699ef0573f749c9c5ee63f30
nniddnoy <73216249+nniddnoy@users.noreply.github.com>
2022-02-18 05:59:25 +0700

    comment

--- html/DQS/application/views/member/v_member_register.php
+++ html/DQS/application/views/member/v_member_register.php
@@ -1,9 +1,10 @@
-+<!-- นางสาวรัชนีกร ป้อชุมภู
-     Date : 23/9/2021 -->
-<!-- แสดงแบบฟอร์มการสมัครสมาชิกของหน้าจอสมัครสมาชิก -->
-<!-- นางสาวรัชนีกร ป้อชุมภู
-     Date : 23/9/2021 -->
-<!-- แสดงแบบฟอร์มการสมัครสมาชิกของหน้าจอสมัครสมาชิก -->
+ <!--* v_member_register
+                * Display show_member_register , 
+                * @input  province, department,prefic,firstname,lastname,email,password,password_confirm
+                * @output show form
+                * @author Ratchaneekorn
+                * @Create Date 2564-09-01
+*/ -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/all.min.css">
 <div class="content">
     <div class="container-fluid" style="padding-top: 100px ;margin: auto;">
@@ -32,6 +33,7 @@
                                 </div>
                             </div>
                             <form action='<?php echo site_url() . 'Member/Member_register/insert_session' ?>'id="form_id" method="post" name='form'>
+                            
                             <!-- เลือกจังหวัดแบบ dropdown list -->
                                 <div class="row gx-5">
                                     <div class="col"><br><br>
@@ -39,10 +41,7 @@
                                         <label style = "color: #000000;">จังหวัด</label>
                                         <label style = "color: #FF0000;">*</label>
                                         
-                                            <!-- <div class="dropdown">
-                                            <select onclick="myFunction()">--------- เลือกจังหวัด ---- -----</select>
-                                            <div id="province_choice" class="dropdown-content">
-                                            <input type="text" placeholder="ค้นหา" id="mem_province_id" onkeyup="filterFunction()"> -->
+                                            
                                         <select  onchange="get_pro()" name="mem_province_id" id="mem_province_id" class="form-select" aria-label="Default select example" required>
                                             <option value="" selected>--------- เลือกจังหวัด ---------</option>
                                             <?php foreach ($arr_province as $value) { ?>
@@ -50,9 +49,8 @@
                                                 
                                             <?php } ?>
                                         </select><br>
-                                        
-                                            
                                 </div>
+                                <!-- เลือกหน่วยงานแบบ dropdown list -->
                                 <div class="col"><br><br>
                                         <label style = "color: #000000;">หน่วยงาน</label>
                                         <label style = "color: #FF0000;">*</label>
@@ -74,11 +72,14 @@
                                         </select><br>
 
                                 </div>
-                                <div class="col">
-                                        <label style = "color: #000000;">ชื่อ</label>
-                                        <label style = "color: #FF0000;">*</label>
-                                        <input type="text" class="form-control" id="mem_firstname" name="mem_firstname" placeholder="ชื่อ"><br>
-                                    </div>
+                                <!-- กรอกชื่อลงในกล่องบันทึกข้อความ -->
+                                <div class="col"> 
+                                    <label style = "color: #000000;">ชื่อ</label>
+                                    <label style = "color: #FF0000;">*</label>
+                                    <input type="text" class="form-control" id="mem_firstname" name="mem_firstname" placeholder="ชื่อ"><br>
+                                </div>
+
+                                    <!-- กรอกชื่อลงในกล่องบันทึกข้อความ -->
                                     <div class="col">
                                         <label style = "color: #000000;">นามสกุล</label>
                                         <label style = "color: #FF0000;">*</label>
@@ -101,8 +102,7 @@
                                         <label for="inputPassword4" style = "color: #000000;">รหัสผ่าน</label>
                                         <label style = "color: #FF0000;">*</label>
                                         <input type="password" class="form-control" id="mem_password" name="mem_password" placeholder="รหัสผ่าน" onchange="checkpassword()" required oninvalid="this.setCustomValidity('โปรดเลือกรหัสผ่านที่ปลอดภัยยิ่งขึ้น ใช้อักขระ 8 ตัวขึ้นไปสำหรับรหัสผ่าน ใช้ตัวอักษร ตัวเลขผสมกัน')" oninput="this.setCustomValidity('')" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}">
-                                            <!-- <i class="show fa fa-eye"></i>
-                                            <i class="hide fa fa-eye-slash"></i> -->
+                                          
                                     </div>
                                     <div class="form-group col-md-4"> <!-- กรอกรหัสยืนยันรหัสผ่านลงใน กล่องบันทึกข้อความ -->
                                         <label for="inputPassword4" style = "color: #000000;">ยืนยันรหัสผ่าน</label>
@@ -138,53 +138,16 @@
 
 
 
-<script type="text/javascript"> //ตรวจสอบการเปิด/ปิดการมองเห็นรหัสผ่าน
-var passwordField = document.querySelector('.password');
-var show = document.querySelector('.show');
-var hide = document.querySelector('.hide');
+<script> 
 
-    function checkpassword() { //ตรวจสอบรหัสผ่านว่าตรงกับรหัสยืนยันหรือไม่
-        const password = document.querySelector('input[name=mem_password]');
-        const confirm = document.querySelector('input[name=confirm_password]');
-        if (confirm.value === password.value) {
-            confirm.setCustomValidity('');
-        } else if (confirm.value !== password.value) {
-            confirm.setCustomValidity('โปรดกรอกรหัสผ่านให้ตรงกัน');
-        }
-    }
-
-show.onclick = function(){ //รหัสผ่านแสดงกดปุ่มแสดงจะปิด
-    passwordField.setAttribute("type","text");
-    show.style.display = "none";
-    hide.style.display = "block";
-}
-
-hide.onclick = function(){//รหัสผ่านถูกปิดการมองเห็นกดปุ่มแสดงจะเปิด
-    passwordField.setAttribute("type","password");
-    hide.style.display = "none";
-    show.style.display = "block";
-}
-
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// function filterFunction() {
-//   var input, filter, ul, li, a, i;
-//   input = document.getElementById("mem_province_id");
-//   filter = input.value.toUpperCase();
-//   div = document.getElementById("province_choice");
-//   a = div.getElementsByTagName("a");
-//   for (i = 0; i < a.length; i++) {
-//     txtValue = a[i].textContent || a[i].innerText;
-//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//       a[i].style.display = "";
-//     } else {
-//       a[i].style.display = "none";
-//     }
-//   }
-
-// }
+/*
+	* email_validation
+	* check email validation
+	* @input mem_email
+	* @output success,error
+	* @author Ratchaneekorn
+	* @Create Date 2565-02-15
+*/
 
 function check_email_input(){
 // $(document).ready(function() {
@@ -209,27 +172,30 @@ function check_email_input(){
                     Swal.fire({
                         icon: 'error',
                         title: 'อีเมลถูกใช้แล้ว',
-                        text: 'กรุณากรอกอีเมลใหม่'
+                        text: 'กรุณากรอกอีเมลใหม่',
+                        confirmButtonText: 'ตกลง'
                     })
                 }
             },
                     
         });
 }
-        
-    // });
-// }); 
 
-
-
-
-function get_dept(value_pro_ID){
+/*
+	* email_validation
+	* check email validation
+	* @input value_pro_id
+	* @output data
+	* @author Ratchaneekorn
+	* @Create Date 2565-02-15
+*/
+function get_dept(value_pro_id){
     $.ajax({
         type: "POST",
       url: "<?php echo site_url() ."Member/Member_register/get_dept_list_ajax" ?>",
       dataType: 'JSON',
       data: {
-            'mem_pro_ID': value_pro_ID
+            'mem_pro_list': value_pro_id
         },
       success:function(data){
           console.log(data); 
@@ -239,7 +205,14 @@ function get_dept(value_pro_ID){
      });
      
 }// recieve json then send to create data table
-
+/*
+	* check_dep_validation
+	* check email validation
+	* @input value_pro_id
+	* @output data
+	* @author Ratchaneekorn
+	* @Create Date 2565-02-15
+*/
 function dep_input(arr_dep) {//
     
         var select = document.getElementById("mem_dep_id");
@@ -261,112 +234,25 @@ function dep_input(arr_dep) {//
             }
     
 }
+/*
+	* _validation
+	* check email validation
+	* @input value_pro_id
+	* @output data
+	* @author Ratchaneekorn
+	* @Create Date 2565-02-15
+*/
 function get_pro(){
-    var mem_pro_ID = document.getElementById("mem_province_id");
-    console.log(mem_pro_ID);
+    var mem_pro_list = document.getElementById("mem_province_id");
+    console.log(mem_pro_list);
     $("#mem_dep_id").empty();
-	get_dept(mem_pro_ID.value);
+	get_dept(mem_pro_list.value);
 }
 
 
 </script>
-<!-- <script type="text/javascript">
-  $('#add-form').submit(function(){
-    $.ajax({
-        type: 'post',
-        url: "<?php echo site_url().'/Member/Member_register/insert_member'?>",
-        data: $( "#add-form" ).serialize(),
-        dataType: 'json',
-        success: function(data) {
-              // console.log("succ");
-              alert('มีชื่อนี้ในระบบอยู่แล้วหรือไม่ได้กรอกข้อมูล กรุณากรอกใหม่');
-        },
-        error: function (error) {
-          // console.log("error");
-          location.reload();
-        }
-    });
-  });
-  </script> -->
 
-  <!-- Edit name -->
-
-<script>
-    // $(document).on("keyup", "#mem_firstname",function() {
-    //     var t = <?php echo json_encode($arr_name) ?>;
-    //     var firstname_input = document.getElementById("mem_firstname");
-    //     var check_name;
-    //     // var div = document.getElementById('target_div');
-    //     var dis_button = document.getElementById('regis');
-
-    //     for (let x in t) {
-    //         if (t[x].mem_firstname == firstname_input.value) {
-                
-    //                 check_name = 1;
-    //                 break;
-                
-    //         } else {
-    //             check_name = 0;
-    //         }
-    //     }
-    //     console.log(check_name);
-    //     if (check_name == 1) {
-    //         $("#mem_firstname").css("border-color", "red");
-            
-    //         // div.style.display = "block";
-    //         dis_button.disabled = true;
-
-    //     } else {
-    //         $("#mem_firstname").css("border-color", "green");
-           
-    //         // div.style.display = "none";
-    //         dis_button.disabled = false;
-
-    //     }
-    // });
-
-     
-    // $(document).ready(function() {
-    //     $('form #regis').click(function(e) {
-    //         let $form = $(this).closest('form');
-    //         $.ajax({
-    //             type: "POST",
-    //             url: "../../Member_register/check_email",
-    //             data: {
-    //                 mem_email: $('#mem_email').val(),
-                    
-    //             },
-                
-    //             success: function(res) {
-    //                 console.log(res);
-    //                 if (res == "true") {
-    //                     // Swal.fire({
-    //                     //     icon: 'success',
-    //                     //     title: 'รีเซ็ตรหัสผ่านเสร็จสิ้น',
-    //                     //     text: 'กำลังกลับไปหน้าหลัก'
-    //                     // })
-                        
-    //                     setTimeout(function() {
-    //                         // document.getElementById('regis').type = 'submit';
-    //                         // $form.submit();
-    //                     }, 1800)
-    //                 } else if (res == "false") {
-    //                     Swal.fire({
-    //                         icon: 'error',
-    //                         title: 'อีเมลนี้ถูกใช้งานแล้ว',
-    //                         text: 'กรุณากรอกอีเมลใหม่'
-    //                     })
-    //                 }
-    //             }
-    //         });
-    //     });
-    // });
-
-    
-</script>
-
-
-  
+<!-- ปรับฟอนต์ให้เป็นไทยสารบัญ   -->
 <style> 
      /*ปรับรูปแบบตัวอักษร */
     @import url('https://fonts.googleapis.com/css2?family=Sarabun&display=swap');
