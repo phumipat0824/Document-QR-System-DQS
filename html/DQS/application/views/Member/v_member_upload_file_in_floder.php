d4303546a6cf92b614ec388c48fa8f811f281eb3
Domeasw <73055457+Domeasw@users.noreply.github.com>
2022-02-19 00:20:25 +0700

    Revert "แก้ไข v_member_home and v_member_home_in_folder"
    This reverts commit faa0bc58624e3299a3ceb743967d8d64bdbb0ba5.

--- html/DQS/application/views/Member/v_member_upload_file_in_floder.php
+++ html/DQS/application/views/Member/v_member_upload_file_in_floder.php
@@ -16,9 +16,8 @@
     <div class="row" style="padding: 100px 10px 10px 20%;">
         <a style="color:#100575; font-size: 80px;">สร้างคิวอาร์โค้ด</a>
         <a style="font-size: 35px;">เริ่มสร้าง QR Code กันเลย </a>
-    </div>
 
-    <div class="row" style="padding-left:20%;">
+
         <div class="col-md-5">
             <div class="card" style="border-color:#E4E4E4;border-width: 5px;">
                 <nav>
@@ -38,11 +37,11 @@
                             <label style="color: #FF0000;">* <span id="text_namef"> </span> </label><br><br>
                             <div class="row">
                                 <div class="col-md-2 offset-md-1">
-                                    <input class="form-control" type="file" id="doc_path" name="doc_path" accept="application/pdf" placeholder="อัปโหลดไฟล์" style="margin: 0px;  width: 500px; height: 50px; padding: 10px;" value="" onchange="file_validation()"><br>
+                                    <input class="form-control" type="file" id="doc_path" name="doc_path" accept="application/pdf" placeholder="อัปโหลดไฟล์" style="padding: 10px; width: 230px; height: 50px;" value="" onchange="file_validation()"><br>
                                     <?php for($i = 0; $i < count($path_fol); $i++){?>
-                                    <?php  if($path_fol[$i] != '@' ){?>
-                                    <input type="hidden" value="<?php echo $path_fol[$i] ?>" id="fol_location_id" name="fol_location_id"></input>
-                                    <?php }?>
+                                        <?php  if($path_fol[$i] != '@' ){?>
+                                            <input type="hidden" value="<?php echo $path_fol[$i] ?>" id="fol_location_id" name="fol_location_id"></input>
+                                        <?php }?>
                                     <?php }?>
                                 </div>
                             </div>
@@ -51,7 +50,7 @@
                             <label style="color: #FF0000;">* <span id="text_name"> </span> </label><br>
                             <div class="row">
                                 <div class="col-md-2 offset-md-1">
-                                    <input type="text" id="doc_name" name="doc_name" style="margin: 0px;  width: 500px;" placeholder="ชื่อไฟล์" value="" onchange="name_validation()"><br />
+                                    <input type="text" class="form-control" id="doc_name" name="doc_name" style="margin: 0px;  width: 400px;" placeholder="ชื่อไฟล์" value="" onchange="name_validation()"><br />
                                 </div>
                             </div>
 
@@ -86,11 +85,11 @@
                             <label style="color: #FF0000;">* <span id="text_nameimgf"> </span> </label><br><br>
                             <div class="row">
                                 <div class="col-md-2 offset-md-1">
-                                    <input class="form-control" type="file" id="doc_pathimg" name="doc_pathimg" accept="image/*" placeholder="อัปโหลดไฟล์" style="margin: 0px;  width: 500px; height: 50px; padding: 10px;" value="" onchange="file_validationimg()"><br>
+                                    <input class="form-control" type="file" id="doc_pathimg" name="doc_pathimg" accept="image/*" placeholder="อัปโหลดไฟล์" style="padding: 10px; width: 230px; height: 50px;" value="" onchange="file_validationimg()"><br>
                                     <?php for($i = 0; $i < count($path_fol); $i++){?>
-                                    <?php  if($path_fol[$i] != '@' ){?>
-                                    <input type="hidden" value="<?php echo $path_fol[$i] ?>" id="fol_location_id2" name="fol_location_id2"></input>
-                                    <?php }?>
+                                        <?php  if($path_fol[$i] != '@' ){?>
+                                            <input type="hidden" value="<?php echo $path_fol[$i] ?>" id="fol_location_id2" name="fol_location_id2"></input>
+                                        <?php }?>
                                     <?php }?>
                                 </div>
                             </div>
@@ -99,7 +98,7 @@
                             <label style="color: #FF0000;">* <span id="text_imgname"> </span> </label><br>
                             <div class="row">
                                 <div class="col-md-2 offset-md-1">
-                                    <input type="text" id="doc_nameimg" name="doc_nameimg" style="margin: 0px;  width: 500px;" placeholder="ชื่อไฟล์" value="" onchange="name_validationimg()"><br />
+                                    <input type="text" id="doc_nameimg" name="doc_nameimg" class="form-control" style="margin: 0px;  width: 400px;" placeholder="ชื่อไฟล์" value="" onchange="name_validationimg()"><br />
                                 </div>
                             </div>
 
@@ -137,7 +136,7 @@
                         <label id="target_div" style="display:none;margin-top: 3%;color: #FF0000;">กรอกข้อมูลลิงค์เว็บไซต์ (URL)</label>
                         <div class="row">
                             <div class="col-md-2 offset-md-1">
-                                <input id="text3" type="text" style="margin: 0px;  width: 500px;" oninvalid="InvalidMsg(this);" oninput="InvalidMsg(this);" value="https://www.example.com" required="required">
+                                <input class="form-control" id="text3" type="text" style="margin: 0px;  width: 400px;" oninvalid="InvalidMsg(this);" oninput="InvalidMsg(this);" value="https://www.example.com" required="required">
                             </div>
                         </div>
                         <div id="inputlogo" style="margin-top: 30px;">
@@ -283,7 +282,7 @@ async function uploadFile() {
         method: "POST",
         data: {
             doc_name: doc_name,
-            fol_location_id: fol_location_id
+            fol_location_id : fol_location_id
         },
         body: formData
     });
@@ -293,7 +292,7 @@ async function uploadFile() {
         method: "POST",
         data: {
             doc_name: doc_name,
-            fol_location_id: fol_location_id
+            fol_location_id : fol_location_id
         },
         body: formData
     });
@@ -327,7 +326,7 @@ async function uploadimg() {
         method: "POST",
         data: {
             doc_nameimg: doc_nameimg,
-            fol_location_id2: fol_location_id2
+            fol_location_id2 : fol_location_id2
         },
         body: formData
 
@@ -338,7 +337,7 @@ async function uploadimg() {
         method: "POST",
         data: {
             doc_nameimg: doc_nameimg,
-            fol_location_id2: fol_location_id2
+            fol_location_id2 : fol_location_id2
         },
         body: formData
     });
