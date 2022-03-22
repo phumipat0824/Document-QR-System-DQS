7afe8a9411f19d12ad77c3da60c45b0206d007f2
lnwnice12 <lnwnice12@gmail.com>
2022-03-22 15:28:10 +0700

    แอดมินอัปโหลดโฟลเดอร์
    แอดมินสามารถอัปโหลดโฟลเดอร์ และจัดการโฟลเดอร์ได้ แต่อัปโหลดไฟล์ไม่ได้แก้ไปแก้ด้วย

--- html/DQS/application/views/Admin/v_admin_home_in_folder.php
+++ html/DQS/application/views/Admin/v_admin_home_in_folder.php
@@ -35,10 +35,10 @@
         <br>
         <div>
             <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" style=" color:#707070; font-weight: 900; font-family:TH Sarabun New; font-size: 25px;" href="<?php echo site_url() . '/Member/Member_home/show_member_home'; ?>">หน้าหลัก</a></li>
+                <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" style=" color:#707070; font-weight: 900; font-family:TH Sarabun New; font-size: 25px;" href="<?php echo site_url() . '/Admin/Admin_home/show_admin_home'; ?>">หน้าหลัก</a></li>
                 <?php for ($i = 0; $i < count($path_fol); $i++) { ?>
                     <?php if ($path_fol[$i] != '@') { ?>
-                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" style="color:#707070; font-weight: 900; font-family:TH Sarabun New; font-size: 25px;" href="<?php echo site_url() . '/Member/Member_home/show_in_folder/66'; ?>"><?php echo $path_fol[$i] ?></a></li>
+                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" style="color:#707070; font-weight: 900; font-family:TH Sarabun New; font-size: 25px;" href="<?php echo site_url() . '/Admin/Admin_home/show_admin_in_folder/66'; ?>"><?php echo $path_fol[$i] ?></a></li>
                     <?php } ?>
                 <?php } ?>
             </ol>
