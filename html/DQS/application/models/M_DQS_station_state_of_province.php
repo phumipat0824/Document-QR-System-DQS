2a564ea6086b8a595eaf8ee268c30dd3b50e0719
nniddnoy <73216249+nniddnoy@users.noreply.github.com>
2022-04-15 23:46:05 +0700

    Update M_DQS_station_state_of_province.php
    ดักโชว์หน่วยงาน

--- html/DQS/application/models/M_DQS_station_state_of_province.php
+++ html/DQS/application/models/M_DQS_station_state_of_province.php
@@ -11,24 +11,13 @@ class M_DQS_station_state_of_province extends Da_DQS_station_state_of_province
         $query = $this->db->query($sql);
         return $query;
     }
-    public function get_station_by_id($state_dep_id)
+    public function get_station_by_id($station_dep_id)
     {
         $sql = "SELECT * FROM {$this->db_name}.DQS_Station_State_of_Province 
         LEFT JOIN {$this->db_name}.DQS_Department 
         ON station_dep_id = dep_id 
-        WHERE station_pro_id = $state_dep_id ";
+        WHERE station_pro_id = $station_dep_id AND station_status = 1";
         $query = $this->db->query($sql);
         return $query;
     }
-    public function checkname($mem_pro_id,$mem_dep_id){
-        $sql = "SELECT * 
-        FROM {$this->db_name}.DQS_Member AS member
-        LEFT JOIN {$this->db_name}.DQS_Station_State_of_Province AS station
-        ON member.mem_pro_id = station.station_pro_id AND member.mem_dep_id = station.station_dep_id
-        WHERE mem_pro_id = '$mem_pro_id' AND station_pro_id = '$mem_pro_id' AND mem_dep_id = '$mem_dep_id' AND station_dep_id = '$mem_dep_id'";
-
-        $query = $this->db->query($sql);
-        return $query;
-    }
-
 }
