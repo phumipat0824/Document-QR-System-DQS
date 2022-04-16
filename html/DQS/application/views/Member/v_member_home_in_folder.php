89405356fde529ca4c8e0975ab1193120f7a95b1
lnwnice12 <73059406+lnwnice12@users.noreply.github.com>
2022-04-16 23:22:29 +0700

    ดักสร้างโฟลเดอร์ ของแอดมิน

--- html/DQS/application/views/Member/v_member_home_in_folder.php
+++ html/DQS/application/views/Member/v_member_home_in_folder.php
@@ -565,7 +565,7 @@
         if (!event.target.matches('.dropbtn')) {
             var dropdowns = document.getElementsByClassName("dropdown-content");
             var i;
-            for (i = 0; i < script dropdowns.length; i++) {
+            for (i = 0; i <  dropdowns.length; i++) {
                 var openDropdown = dropdowns[i];
                 if (openDropdown.classList.contains('show')) {
                     openDropdown.classList.remove('show');
