dd29af6fc58d16b3e32d76d0bcaf1cfc668620fe
Domeasw <73055457+Domeasw@users.noreply.github.com>
2022-04-20 17:16:09 +0700

    Update v_admin_home_in_folder.php

--- html/DQS/application/views/Admin/v_admin_home_in_folder.php
+++ html/DQS/application/views/Admin/v_admin_home_in_folder.php
@@ -424,6 +424,44 @@
 
     });
 
+    document.getElementById("download").addEventListener("click", function() {
+
+
+        const note = document.querySelector('#qr_path');
+        note.style.border = '10px solid #fff';
+
+        html2canvas(document.querySelector('#qr_path')).then(function(canvas) {
+
+            saveAs(canvas.toDataURL(), 'DQS_QR.png');
+        });
+
+    });
+
+    function saveAs(uri, filename) {
+
+        var link = document.createElement('a');
+
+        if (typeof link.download === 'string') {
+
+            link.href = uri;
+            link.download = filename;
+
+            //Firefox requires the link to be in the body
+            document.body.appendChild(link);
+
+            //simulate click
+            link.click();
+
+            //remove the link when done
+            document.body.removeChild(link);
+
+        } else {
+
+            window.open(uri);
+
+        }
+    }
+
     function set_delete(path, id, fol_id) {
         $('#doc_path_delete').val(path);
         $('#doc_id_delete').val(id);
@@ -1028,43 +1066,5 @@
 
         }
 
-    }
-
-    document.getElementById("download").addEventListener("click", function() {
-
-
-        const note = document.querySelector('#qr_path');
-        note.style.border = '10px solid #fff';
-
-        html2canvas(document.querySelector('#qr_path')).then(function(canvas) {
-
-            saveAs(canvas.toDataURL(), 'DQS_QR.png');
-        });
-
-    });
-
-    function saveAs(uri, filename) {
-
-        var link = document.createElement('a');
-
-        if (typeof link.download === 'string') {
-
-            link.href = uri;
-            link.download = filename;
-
-            //Firefox requires the link to be in the body
-            document.body.appendChild(link);
-
-            //simulate click
-            link.click();
-
-            //remove the link when done
-            document.body.removeChild(link);
-
-        } else {
-
-            window.open(uri);
-
-        }
     }
     </script>
