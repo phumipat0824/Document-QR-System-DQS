2a36301a3f5d8a19cb762cbb4bcaf8ee13cf5ce8
nniddnoy <73216249+nniddnoy@users.noreply.github.com>
2022-04-23 04:00:02 +0700

    Update status

--- html/DQS/application/models/Da_DQS_station_state_of_province.php
+++ html/DQS/application/models/Da_DQS_station_state_of_province.php
@@ -15,7 +15,7 @@ class Da_DQS_station_state_of_province extends DQS_model
     {
         $sql = "UPDATE {$this->db_name}.DQS_Station_State_of_Province
         SET station_status = 0
-        WHERE station_pro_id = $mem_pro_id && station_dep_id = 1";
+        WHERE station_pro_id = $mem_pro_id && station_dep_id = $mem_dep_id";
         $this->db->query($sql, array($this-> station_status));
     }
 
